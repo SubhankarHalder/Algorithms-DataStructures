4c2cc9b0e39e5f22768e5b640367f57c889e7150
SubhankarHalder <subhankarvirgoan@gmail.com>
2020-08-19 06:45:55 +0530

    Add linked list size file

---
+++ solutions/linked_list_size.py
@@ -0,0 +1,38 @@
+class Node:
+    def __init__(self, initdata):
+        self.data = initdata
+        self.next = None
+
+    def get_data(self):
+        return self.data
+
+    def get_next(self):
+        return self.next
+
+    def set_data(self, newdata):
+        self.data = newdata
+
+    def set_next(self, newnext):
+        self.next = newnext
+
+
+class Unordered_List:
+    def __init__(self):
+        self.head = None
+
+    def is_empty(self):
+        return self.head == None
+
+    def add(self, data):
+        temp = Node(data)
+        temp.set_next(self.head)
+        self.head = temp
+
+    def size(self):
+        current = self.head
+        count = 0
+        while current != None:
+            count += 1
+            current = current.get_next()
+
+        return count
